ad7bdeb1fd4a9ffc80162341dd93eddbd20cbbc5
sahil tagala <sahiltagala1@gmail.com>
2023-04-01 00:49:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className='container'>
+        <CurrentLocation />
+      </div>
+      <div className='footer-info'>
+        Developed by{' '}
+        <a target='_blank' href='https://github.com/sahiltagala1'>
+          Sahil Tagala
+        </a>{' '}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "032ab5120a6b8f329d29c18e2e94e1b1",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
